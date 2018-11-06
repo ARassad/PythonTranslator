0da610a014bef5e65a09221e60cb86828b88d428
Artem Kurylev <artembonapart@gmail.com>
2018-11-06 23:00:22 +0300

    Исправлены синтаксические ошибки, функция вывода в дерево вынесена в отдельный файл

---
+++ Project1/printTree.h
@@ -0,0 +1,8 @@
+#include "tree_struct.h"
+#include <stdio.h>
+#include <malloc.h>
+//#include "python.bison_tab.h"
+void printTree(void);
+void printStmt(int parentID, struct Statement* stmt);
+void printExpr(int parentID, struct Expression* expr, int* maxId);
+void printList(int parentID, struct List* list, int* maxId);
