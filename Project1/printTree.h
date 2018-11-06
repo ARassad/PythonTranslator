339d4eb71a4476402584fa2c71c9d678aaa2db04
Artem Kurylev <artembonapart@gmail.com>
2018-11-07 00:45:14 +0300

    Исправил заголовок

--- Project1/printTree.h
+++ Project1/printTree.h
@@ -3,6 +3,6 @@
 #include <malloc.h>
 //#include "python.bison_tab.h"
 void printTree(void);
-void printStmt(int parentID, struct Statement* stmt);
+void printStmt(int parentID, struct Statement* stmt, int* maxId);
 void printExpr(int parentID, struct Expression* expr, int* maxId);
 void printList(int parentID, struct List* list, int* maxId);
