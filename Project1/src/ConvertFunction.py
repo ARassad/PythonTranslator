fbf1573dbdbed03f35f05307661dcda516d767f8
Artem Kurylev <artembonapart@gmail.com>
2018-12-20 14:36:55 +0400

    Теперь в сворачивается в main_function

--- Project1/src/ConvertFunction.py
+++ Project1/src/ConvertFunction.py
@@ -1,5 +1,6 @@
 import Classes as cl
 import Enums as en
+import List as lt
 from Enums import ExprType, ListType, StmtType
 import uuid
 
@@ -18,8 +19,24 @@ def create_main_class(root):
     main_class.identifier.stringVal = "<MainClass>"
 
     main_class.firstSuite = root
-
+    create_main_function(main_class)
     return main_class
 
 
+def create_main_function(root):
+    main_funct = cl.STMT()
+    main_funct.type = StmtType.ST_FUNCTION_DEF
+
+    main_funct.identifier = cl.EXPR()
+    main_funct.identifier.type = ExprType.ET_ID
+    main_funct.identifier.stringVal = "main"
+    main_funct.stmtList = lt.List()
+    main_funct.stmtList.type = ListType.LT_EXPR_FUNCTION_PARAMS
+    main_funct.stmtList.expr = cl.EXPR()
+    main_funct.stmtList.expr.type = ExprType.ET_FUNC_PARAM
+    main_funct.stmtList.expr.identifier = cl.EXPR()
+    main_funct.stmtList.expr.identifier.stringVal = "args"
+    main_funct.stmtList.expr.identifier.type = ExprType.ET_ID
+    main_funct.firstSuite = root.firstSuite
+    root.firstSuite = main_funct
 
