69a69d4071b9074fecd09f55ea671ee325eb88a4
Arkadi <arassadnikov97@gmail.com>
2018-12-21 21:02:08 +0300

    Method

---
+++ Project1/rtl/src/std/__PyMethod.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package std;
+
+/**
+ *
+ * @author Arkadi
+ */
+public class __PyMethod extends __PyObject{
+
+    public __PyMethod() {
+    }
+    
+    public __PyObject __call__() throws Exception {
+        return this;
+    }
+}
