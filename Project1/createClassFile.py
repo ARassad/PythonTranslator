66a7358c2fed26464d94551721aa6f25d9afa795
Artem Kurylev <artembonapart@gmail.com>
2018-12-22 20:47:06 +0400

    Обновил генеарцию класс файла

--- Project1/createClassFile.py
+++ Project1/createClassFile.py
@@ -18,8 +18,8 @@ def create_version():
 
 
 # Печать таблицы констант
-def print_constant_table():
-    prog = da.doAll()
+def print_constant_table(prog):
+
     num = prog.constant_table.length + 1
     text = bytearray(num.to_bytes(1, byteorder='big'))
     writefile.write(bytes(text))
@@ -46,23 +46,28 @@ def print_constant_table():
         elif constant.ctype == ECONSTANT.Float:
             text += bytearray(constant.value.to_bytes(1, byteorder='big'))
         elif constant.ctype == ECONSTANT.NameAndType:
-            text += bytearray(constant.index.to_bytes(1, byteorder='big'))
+            text += bytearray(constant.value[0].to_bytes(1, byteorder='big')) + \
+                    bytearray(constant.value[1].to_bytes(1, byteorder='big'))
         elif constant.ctype == ECONSTANT.Class:
-            text += bytearray(constant.index.to_bytes(1, byteorder='big'))
+            text += bytearray(constant.value[0].to_bytes(1, byteorder='big'))
         elif constant.ctype == ECONSTANT.Fieldref:
-            text += bytearray(constant.index.to_bytes(1, byteorder='big'))
+            text += bytearray(constant.value[0].to_bytes(1, byteorder='big')) + \
+                    bytearray(constant.value[1].to_bytes(1, byteorder='big'))
         elif constant.ctype == ECONSTANT.Methodref:
-            text += bytearray(constant.index.to_bytes(1, byteorder='big'))
+            text += bytearray(constant.value[0].to_bytes(1, byteorder='big')) + \
+                    bytearray(constant.value[1].to_bytes(1, byteorder='big'))
         writefile.write(text)
+
 
 def print_access_flags():
     text = '\x02\x01'
     writefile.write(bytes(text.encode('ansi')))
 
 
-def print_class():
+def print_class(prog):
     # Здесь надо напечать номер константы с именем класса с типом CONSTANT CLASS
-    text = bytearray(int(1).to_bytes(1, byteorder='big'))
+
+    text = int(1).to_bytes(1, byteorder='big')
     writefile.write(text)
 
 
@@ -85,20 +90,22 @@ def print_fields():
 
 
 # Сначала напечатать кол-во полей, потом сами методы
-def print_methods():
-    text = bytearray(int(0).to_bytes(1, byteorder='big'))
+def print_methods(prog):
+
+    text = bytearray(int(1).to_bytes(1, byteorder='big'))
     writefile.write(text)
 
 
 if __name__ == '__main__':
+    prog = da.doAll()
     create_magical_number()
     create_version()
-    print_constant_table()
+    print_constant_table(prog)
     print_access_flags()
-    print_class()
+    print_class(prog)
     print_parent()
     print_count_interfaces()
     print_fields()
-    print_methods()
+    print_methods(prog)
 
 
